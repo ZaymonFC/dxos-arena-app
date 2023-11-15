db4ca4984b1d89ed295b6927b37a3b26e281a6cd
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-11-15 14:20:44 -0800

    ✨ Add game logic

---
+++ src/lib/game.test.ts
@@ -0,0 +1,80 @@
+import { test, expect } from "vitest";
+import { applyAction } from "./useStore";
+import { GameAction, GameState, exec, zeroState } from "./game";
+
+// --- HELPERS ----------------------------------------------------------------
+const applyMany = (state: GameState, actions: GameAction[]) => {
+  let nextState = state;
+
+  for (const action of actions) {
+    const { nextState: statePrime } = applyAction(nextState, action, exec);
+    nextState = statePrime;
+  }
+
+  return nextState;
+};
+
+// --- TEST CASES -------------------------------------------------------------
+
+test("Can create a game", () => {
+  const state = applyMany(zeroState, [
+    { type: "game-created", payload: { players: { white: "zan", black: "zhenya" } } },
+  ]);
+
+  expect(state.players.white).toBe("zan");
+  expect(state.players.black).toBe("zhenya");
+});
+
+test("Can play a move", () => {
+  const state = applyMany(zeroState, [
+    { type: "game-created", payload: { players: { white: "zan", black: "zhenya" } } },
+    { type: "move-made", payload: { source: "e2", target: "e4" } },
+  ]);
+
+  expect(state.moves).toHaveLength(1);
+  expect(state.moves[0].source).toBe("e2");
+  expect(state.status).toBe("in-progress");
+});
+
+test("White can resign", () => {
+  const state = applyMany(zeroState, [
+    { type: "game-created", payload: { players: { white: "zan", black: "zhenya" } } },
+    { type: "move-made", payload: { source: "e2", target: "e4" } },
+    { type: "move-made", payload: { source: "e7", target: "e5" } },
+    { type: "player-resigned", payload: { player: "white" } },
+  ]);
+
+  expect(state.status).toBe("complete");
+  expect(state.gameOverReason).toBe("white-resignation");
+});
+
+test("Moves after game over should be ignored", () => {
+  const state = applyMany(zeroState, [
+    { type: "game-created", payload: { players: { white: "zan", black: "zhenya" } } },
+    { type: "move-made", payload: { source: "e2", target: "e4" } },
+    { type: "move-made", payload: { source: "e7", target: "e5" } },
+    { type: "player-resigned", payload: { player: "white" } },
+
+    // knight f3, knight c6
+    { type: "move-made", payload: { source: "g1", target: "f3" } },
+    { type: "move-made", payload: { source: "b8", target: "c6" } },
+  ]);
+
+  expect(state.moves).toHaveLength(2);
+});
+
+test("Scholars mate should end the game in checkmate", () => {
+  const state = applyMany(zeroState, [
+    { type: "game-created", payload: { players: { white: "zan", black: "zhenya" } } },
+    { type: "move-made", payload: { source: "e2", target: "e4" } },
+    { type: "move-made", payload: { source: "e7", target: "e5" } },
+    { type: "move-made", payload: { source: "f1", target: "c4" } },
+    { type: "move-made", payload: { source: "b8", target: "c6" } },
+    { type: "move-made", payload: { source: "d1", target: "h5" } },
+    { type: "move-made", payload: { source: "g8", target: "f6" } },
+    { type: "move-made", payload: { source: "h5", target: "f7" } },
+  ]);
+
+  expect(state.status).toBe("complete");
+  expect(state.gameOverReason).toBe("checkmate");
+});
---
+++ src/lib/game.ts
@@ -0,0 +1,140 @@
+import { Chess } from "chess.js";
+import { produce } from "immer";
+import { match } from "ts-pattern";
+
+type Move = {
+  source: string;
+  target: string;
+  promotion?: "q" | "r" | "b" | "n";
+};
+
+type GameOverReason =
+  | "checkmate"
+  | "white-resignation"
+  | "black-resignation"
+  | "stalemate"
+  | "insufficient-material"
+  | "threefold-repetition";
+
+// todo(zan): Time
+
+type GameStatus = "waiting" | "in-progress" | "complete";
+
+// todo(zan): Think about time control
+export type GameState = {
+  variant: "standard";
+  moves: Move[];
+  boards: string[];
+  players?: { white: string; black: string };
+  status: GameStatus;
+  gameOverReason?: GameOverReason;
+};
+
+export const zeroState: GameState = {
+  variant: "standard",
+  moves: [],
+  boards: [new Chess().fen()],
+  status: "waiting",
+};
+
+export type GameAction =
+  | {
+      type: "game-created";
+      payload: { players: { white: string; black: string } };
+    }
+  | { type: "move-made"; payload: Move }
+  | {
+      type: "takeback-requested";
+      payload: { player: "white" | "black"; moveNumber: number };
+    }
+  | { type: "takeback-accepted"; payload: {} }
+  | { type: "player-resigned"; payload: { player: "white" | "black" } }
+  | { type: "game-over"; payload: GameOverReason };
+
+export const exec = (state: GameState, action: GameAction): [GameState, GameAction[]] => {
+  let actions: GameAction[] = [];
+
+  const newState = produce(state, (draft) => {
+    switch (action.type) {
+      case "game-created": {
+        draft.players = action.payload.players;
+        break;
+      }
+
+      case "move-made": {
+        try {
+          // TODO(zan): To support variants, we can use a different rules engine
+          const chess = new Chess(state.boards[state.boards.length - 1]);
+
+          chess.move({
+            from: action.payload.source,
+            to: action.payload.target,
+            promotion: "q",
+          });
+
+          // If first move is made, game is in progress
+          if (state.moves.length === 0) {
+            draft.status = "in-progress";
+          }
+
+          if (draft.status !== "in-progress") {
+            break;
+          }
+
+          draft.moves.push(action.payload);
+          draft.boards.push(chess.fen());
+
+          if (chess.isGameOver) {
+            if (chess.isCheckmate()) {
+              actions.push({ type: "game-over", payload: "checkmate" });
+            }
+            if (chess.isStalemate()) {
+              actions.push({ type: "game-over", payload: "stalemate" });
+            }
+
+            if (chess.isInsufficientMaterial()) {
+              actions.push({ type: "game-over", payload: "insufficient-material" });
+            }
+
+            if (chess.isThreefoldRepetition()) {
+              actions.push({ type: "game-over", payload: "threefold-repetition" });
+            }
+          }
+        } catch (e) {
+          console.log("Invalid move");
+        }
+
+        break;
+      }
+
+      case "takeback-requested":
+        break;
+
+      case "takeback-accepted":
+        break;
+
+      case "player-resigned": {
+        const { player } = action.payload;
+
+        actions.push({
+          type: "game-over",
+          payload: match(player)
+            .with("white", () => "white-resignation")
+            .with("black", () => "black-resignation")
+            .exhaustive() as GameOverReason,
+        });
+        break;
+      }
+
+      case "game-over": {
+        draft.status = "complete";
+        draft.gameOverReason = action.payload;
+
+        break;
+      }
+    }
+    return draft;
+  });
+
+  return [newState, actions];
+};
