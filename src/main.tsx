90470c3dd7e93adc87d1cc5b76f8977e7c330230
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-11-24 14:49:00 -0800

    🧹 Use react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
 // This includes css styles from @dxos/react-ui-theme.
 // This must precede all other style imports in the app.
@@ -7,4 +8,8 @@ import "@dxosTheme";
 
 import { App } from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
